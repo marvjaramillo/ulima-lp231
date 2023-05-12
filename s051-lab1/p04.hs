33a2e83a6c80a4cd608e939b7a36b8303a0a5a0a
jbrcaballero <jbrcaballero@gmail.com>
2023-05-12 18:40:50 -0500

    Correccion ejercicio 04

--- s051-lab1/p04.hs
+++ s051-lab1/p04.hs
@@ -1,13 +1,6 @@
-convertirBinario 0 = [0]
-convertirBinario num =	[(mod num 2)] ++ convertirBinario (div num 2)
+main = print (checkTransmission [1, 1, 1, 1])
 
-contarNumMod lista = contarNumMod' lista 0 contarNumMod' lista contador =
-if(lista==[])
-then mod contador 4
-else contarNumMod' (tail lista) (contador + (head lista))
-
-
-agregarBitAdicionalCuatro lista = agregarBitAdicionalCuatro' lista 0 [] (contarNumMod lista)
-agregarBitAdicionalCuatro' lista unos listaAux contador = if(lista==[])
-then listaAux ++ (convertirBinario contador)
-else agregarBitAdicionalCuatro' (tail lista) (unos+1) (listaAux ++ [(head lista)]) contador
+checkTransmission list = checkTransmission' list 0
+    where
+        checkTransmission' [x] count = x == (mod count 2)
+        checkTransmission' (x:xs) count = checkTransmission' xs (count + x)
