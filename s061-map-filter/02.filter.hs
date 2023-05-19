7f606fee215989d509ca813876f54fdbe1a0954f
jbrcaballero <jbrcaballero@gmail.com>
2023-05-19 18:07:44 -0500

    Ejercicios prolog

--- s061-map-filter/02.filter.hs
+++ s061-map-filter/02.filter.hs
@@ -1,7 +1,7 @@
 main = do
     --Filtrar los elementos de 3 o mas cifras de la lista 
     -- "values" que almacena numeros positivos
-    let values = [10, 200, 124]
+    let values = [10, 200, 124, 88, 1240, 18, 1]
     print (filter (\x -> x >= 100) values)
     let totalCount = length (values)
     let filteredCount = length (filter (\x -> x >= 100) values)
@@ -9,5 +9,4 @@ main = do
     print $ "Total valores: " ++ show totalCount
     print $ "De 3 cifras: " ++ show filteredCount
     print $ "Porcentaje 3 cifras: " ++ show percentage ++ " %"
-
     
